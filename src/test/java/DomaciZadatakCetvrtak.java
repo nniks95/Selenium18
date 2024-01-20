84818c5c5bb79192a1cf2da9d28ed6feaa768b08
nniks95 <nikola.nikolic.it@gmail.com>
2024-01-20 19:51:15 +0100

    new instances

--- src/test/java/DomaciZadatakCetvrtak.java
+++ src/test/java/DomaciZadatakCetvrtak.java
@@ -1,9 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.*;
 import java.time.Duration;
 
@@ -11,9 +10,8 @@ import java.time.Duration;
 public class DomaciZadatakCetvrtak {
 
 
-    //Koristeci Anotacije - Ulogujte se na demoqa(https://demoqa.com/ -> Book Store Application)
-    // preko cookies-a, dodati dve knjige na svoj nalog, zatim se izlogovati brisanjem cookies-a.
-    //Ulogovati se ponovo preko log-in forme i potvrditi da se knjige i dalje nalaze na nalogu
+    //Ja sam oba slucaja radio preko logina, jer preko cookie-a nisam uspeo da se ulogujem
+    //program u sustini radi, samo nisam bio siguran jel za svako dodavanje knjige trebao poseban test
 
     //ItBootCamp23!@#
     //nniks95
@@ -24,6 +22,9 @@ public class DomaciZadatakCetvrtak {
     WebDriverWait wait;
     WebElement usernameField;
     WebElement passwordField;
+    JavascriptExecutor jse;
+    WebElement bookGitPocketGuide;
+    WebElement bookLearningJavaScriptDesign;
 
     @BeforeClass
     public void setUp() throws InterruptedException {
@@ -34,6 +35,7 @@ public class DomaciZadatakCetvrtak {
         wait = new WebDriverWait(wd, Duration.ofSeconds(10));
         username = "nniks95";
         password = "ItBootCamp23!@#";
+        jse = (JavascriptExecutor)wd;
 
 
     }
@@ -46,21 +48,53 @@ public class DomaciZadatakCetvrtak {
         loginButton.click();
         usernameField = wd.findElement(By.id("userName"));
         passwordField = wd.findElement(By.id("password"));
-
     }
     @Test
-    public void test1() throws InterruptedException {
+    public void addFirstBookToCart() throws InterruptedException {
         usernameField.sendKeys(username);
         passwordField.sendKeys(password);
         WebElement loginButton1 = wd.findElement(By.id("login"));
         loginButton1.click();
-        WebElement bookGitPocketGuide = wd.findElement(By.linkText("Git Pocket Guide"));
+        bookGitPocketGuide = wd.findElement(By.id("see-book-Git Pocket Guide"));
         bookGitPocketGuide.click();
         Thread.sleep(1000);
+        jse.executeScript("window.scrollBy(0,350)");
         WebElement addToYourCollectionButton = wd.findElement(By.cssSelector(".text-right.fullButton"));
         addToYourCollectionButton.click();
+        wd.navigate().refresh();
+        Thread.sleep(1000);
+        WebElement backToBookStoreButton  = wd.findElement(By.cssSelector(".text-left.fullButton"));
+        backToBookStoreButton.click();
+        Thread.sleep(1000);
+        bookLearningJavaScriptDesign = wd.findElement(By.id("see-book-Learning JavaScript Design Patterns"));
+        bookLearningJavaScriptDesign.click();
+
+        Thread.sleep(1000);
+        jse.executeScript("window.scrollBy(0,350)");
+        WebElement addToYourCollectionButton2 = wd.findElement(By.cssSelector(".text-right.fullButton"));
+        addToYourCollectionButton2.click();
+        wd.navigate().refresh();
+        wd.manage().deleteAllCookies();
+
+    }
+    @Test
+    public void addSecondBookToCart() throws InterruptedException{
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        WebElement loginButton1 = wd.findElement(By.id("login"));
+        loginButton1.click();
+        Thread.sleep(2000);
+        jse.executeScript("window.scrollBy(0,600)");
+        WebElement profilPage = wd.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div[1]/div/div/div[6]/div/ul/li[3]/span"));
+        profilPage.click();
+        Thread.sleep(1000);
+        Assert.assertTrue(wd.findElement(By.id("see-book-Learning JavaScript Design Patterns")).isDisplayed());
+        Assert.assertTrue(wd.findElement(By.id("see-book-Git Pocket Guide")).isDisplayed());
 
 
-
+    }
+    @AfterClass
+    public void tearDown(){
+        wd.quit();
     }
 }
